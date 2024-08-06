d987469ef678905f931c7d5b18a040e3386cb0e3
Christopher James <68414004+Mindful-Developer@users.noreply.github.com>
2024-08-06 13:51:26 -0400

    Make header mobile-friendly

---
+++ public/scripts/mobile-menu.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const mobileMenuButton = document.getElementById('mobile-menu-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    if (mobileMenuButton && mobileMenu) {
+        mobileMenuButton.addEventListener('click', function() {
+            mobileMenu.classList.toggle('hidden');
+            mobileMenu.classList.toggle('flex');
+            mobileMenu.classList.toggle('justify-evenly');
+        });
+    }
+});
--- src/components/layout/BaseHtml.tsx
+++ src/components/layout/BaseHtml.tsx
@@ -3,18 +3,20 @@ export default function BaseHtml(props: {
 }) {
 	return (
 		<html lang="en" class="bg-gray-900 text-white">
-			<head>
-				<meta charset="UTF-8" />
-				<meta
-					name="viewport"
-					content="width=device-width, user-sc00alable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
-				/>
-				<title> Document </title>
-				<script src="/public/scripts/htmx2.0.1.min.js"></script>
-				<script src="/public/scripts/footer-animation.js"></script>
-				<link rel="stylesheet" href="/public/styles.css" />
-			</head>
-			<body class="flex flex-col w-full h-screen items-center box-border p-2 bg-[#161616] font-['Roboto-Regular'] text-[#d9d9d9]">
+		<head>
+			<meta charset="UTF-8"/>
+			<meta
+				name="viewport"
+				content="width=device-width, user-sc00alable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0"
+			/>
+			<title> Document </title>
+			<script src="/public/scripts/htmx2.0.1.min.js"></script>
+			<script src="/public/scripts/footer-animation.js"></script>
+			<script src="/public/scripts/mobile-menu.js"></script>
+			<link rel="stylesheet" href="/public/styles.css"/>
+		</head>
+		<body
+			class="flex flex-col w-full h-screen items-center box-border p-2 bg-[#161616] font-['Roboto-Regular'] text-[#d9d9d9]">
 				{props.children}
 			</body>
 		</html>
