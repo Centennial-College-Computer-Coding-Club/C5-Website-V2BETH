5d8d5c5969aa27106ddf1694813951c3b0fd80ea
Christopher James <68414004+Mindful-Developer@users.noreply.github.com>
2024-08-06 13:00:30 -0400

    Link logo to home

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 export default function Header({ currentPath }: { currentPath: string }) {
     return (
         <header class="flex h-20 w-full items-center justify-between sticky top-0 mb-16 text-lg bg-[#161616] z-10 px-4">
-            <img class="h-16 w-16" src="/public/imgs/C5logo.png" alt="C5 logo"/>
+            <a href="/">
+                <img class="h-16 w-16" src="/public/imgs/C5logo.png" alt="C5 logo"/>
+            </a>
             <nav class="flex absolute left-1/2 -translate-x-1/2 gap-2">
                 <a href="/" class={currentPath === "home" ? "button-secondary" : "button-tertiary"}>Home</a>
                 <a href="/about" class={currentPath === "about" ? "button-secondary" : "button-tertiary"}>About</a>
