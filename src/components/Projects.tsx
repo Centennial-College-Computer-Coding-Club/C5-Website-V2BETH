80cd6c9ae7541e66d96fe51aa18fb6500f66152b
Christopher James <68414004+Mindful-Developer@users.noreply.github.com>
2024-08-07 14:58:29 -0400

    fix styling for projects grid

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,7 +9,6 @@ export default function Projects() {
             </p>
             <div
                 id="projects-list"
-                class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full"
                 hx-get="/api/projects"
                 hx-trigger="load"
                 hx-indicator="#projects-loading"
