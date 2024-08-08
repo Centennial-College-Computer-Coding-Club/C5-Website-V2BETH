38b124c398612d794746ca587b12a92354729fc9
Christopher James <68414004+Mindful-Developer@users.noreply.github.com>
2024-08-07 20:38:09 -0400

    make header always visible

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header({ currentPath }: { currentPath: string }) {
     return (
-        <header class="flex h-20 w-full items-center justify-between text-lg bg-[#161616] z-10 px-4">
+        <header class="flex h-20 w-full items-center justify-between text-lg bg-[#161616] z-10 px-4 fixed top-0">
             <a href="/" class="flex items-center">
                 <img class="h-16 w-16" src="/public/imgs/c5-logo.png" alt="C5 logo"/>
             </a>
